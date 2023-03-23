14002eaac83f9adce7e38f8524f90d65f2db165d
kewton-carmo <kewton_venancio@atlantico.com.br>
2023-03-23 12:44:56 -0300

    [Main] Included RNW 0.71

---
+++ windows/HPXtest0672T/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::HPXtest0672T::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::HPXtest0672T::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
